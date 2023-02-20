c09ca7a1f31ac65263b101900d8e9e8427f87866
janoujan <janoujan@laposte.net>
2023-02-20 20:25:36 +0100

    rebuild dist and tide up app

--- app.js
+++ app.js
@@ -24,52 +24,52 @@ mongoose
 
 mongoose.plugin(mongodbErrorHandler)
 
-app.use((req, res, next) => {
-  // Setting the nonce on response object to be used later
-  res.locals.nonce = uuidv4().replace(/\-/g, '')
-  // Defining the CSP middleware
-  const cspMiddleWare = helmet.contentSecurityPolicy({
-    useDefaults: true,
-    directives: {
-      'style-src': ["'self'", `'nonce-${res.locals.nonce}'`]
-    }
-  })
-  cspMiddleWare(req, res, next)
-})
-app.get('/', function (req, res) {
-  // Whenever the index page is requested, attach the generated nonce by replacing a keyword
-  const filePath = path.resolve(__dirname, 'dist', 'index.html')
-  // read in the index.html file
-  fs.readFile(filePath, 'utf8', function (err, data) {
-    if (err) {
-      return console.log(err)
-    }
-    // replace the unique keyword (in this case 'random-csp-nonce') with server generated nonce
-    result = data.replace('random-csp-nonce', res.locals.nonce)
-    res.send(result)
-  })
-})
+// app.use((req, res, next) => {
+//   // Setting the nonce on response object to be used later
+//   res.locals.nonce = uuidv4().replace(/\-/g, '')
+//   // Defining the CSP middleware
+//   const cspMiddleWare = helmet.contentSecurityPolicy({
+//     useDefaults: true,
+//     directives: {
+//       'style-src': ["'self'", `'nonce-${res.locals.nonce}'`]
+//     }
+//   })
+//   cspMiddleWare(req, res, next)
+// })
+// app.get('/', function (req, res) {
+//   // Whenever the index page is requested, attach the generated nonce by replacing a keyword
+//   const filePath = path.resolve(__dirname, 'dist', 'index.html')
+//   // read in the index.html file
+//   fs.readFile(filePath, 'utf8', function (err, data) {
+//     if (err) {
+//       return console.log(err)
+//     }
+//     // replace the unique keyword (in this case 'random-csp-nonce') with server generated nonce
+//     result = data.replace('random-csp-nonce', res.locals.nonce)
+//     res.send(result)
+//   })
+// })
 
 app.options('*', cors())
 
 app
   .use(express.json())
   .use(cors())
-  .use(helmet({ crossOriginResourcePolicy: { policy: 'cross-origin' } }))
+  .use(helmet())
   .use(mongoSanitize({ replaceWith: '_' }))
 
 app.use('/api/auth', userRoutes)
 app.use('/api/sauces', saucesRoutes)
 app.use('/images', express.static(path.join(__dirname, 'images')))
 
-// adapt header for cors
-app.use((req, res, next) => {
-  res.setHeader(
-    'Content-Security-Policy',
-    "default-src 'self'; script-src 'self' 'unsafe-inline';"
-  )
-  next()
-})
+// // adapt header for cors
+// app.use((req, res, next) => {
+//   res.setHeader(
+//     'Content-Security-Policy',
+//     "default-src 'self' 'unsafe-inline'; script-src 'self' 'unsafe-inline'; img-src 'self' 'unsafe-inline';"
+//   )
+//   next()
+// })
 
 // Serve the static files from the dist directory
 app.use(express.static(path.join(__dirname, 'dist')))
