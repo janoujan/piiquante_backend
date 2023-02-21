14dfee95923e63aa4ce22241cdd1dd1c9c21d370
janoujan <janoujan@laposte.net>
2023-02-21 12:29:49 +0100

    try to open blocking csp properties

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ app.use('/images', express.static(path.join(__dirname, 'images')))
 app.use((req, res, next) => {
   res.setHeader(
     'Content-Security-Policy',
-    "script-src-attr 'unsafe-inline'; img-src 'https://hotsauce-378315.oa.r.appspot.com/*' 'http://localhost:3000/*';"
+    "script-src-attr 'unsafe-inline'; img-src 'https://*hotsauce-378315.oa.r.appspot.com/' 'http://*localhost:3000/' 'unsafe-inline'git;"
   )
   next()
 })
